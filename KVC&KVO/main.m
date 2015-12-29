6f71d7f3084ff78dc922dd20ebbae39a93b745d4
hongchenxi <hongchenxi86@gmail.com>
2015-12-29 09:56:37 +0800

    Initial Commit

---
+++ KVC&KVO/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KVC&KVO
+//
+//  Created by 洪晨希 on 15/12/29.
+//  Copyright © 2015年 洪晨希. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
